d83ee5068843fa622f9f601a76af747a86677ccb
Sergii Trunov <Sergii_Trunov@epam.com>
2022-07-12 11:07:52 +0300

    removed extra chart

--- purchase-statistic-app/src/app/components/home/home.component.ts
+++ purchase-statistic-app/src/app/components/home/home.component.ts
@@ -10,7 +10,7 @@ import { WebSocketService } from 'src/app/services/webSocket.service';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit, AfterViewInit {
+export class HomeComponent implements OnInit {
   purchases!: Purchase[];
 
   @ViewChild('purchstat') purchStatChart!: BrandHealthChartComponent;
@@ -20,13 +20,6 @@ export class HomeComponent implements OnInit, AfterViewInit {
 
   constructor(private service: PurchasesService, private webSocketService: WebSocketService) { }
 
-  ngAfterViewInit(): void {
-    const brandHealt = { meaningful: 152, different: 95, salient: 168, power: 16.4 };
-    
-    const chartData = this.processHealtData(brandHealt);
-
-    this.brandHealthChart.drawChart(chartData);
-  }
 
   ngOnInit(): void {
     this.service.getPurchases().subscribe({
@@ -110,62 +103,6 @@ export class HomeComponent implements OnInit, AfterViewInit {
 
     return chartData;
 
-  }
-
-  private processHealtData(brandHealt: any): ChartData {
-    const meaningFulAngel = (135/360) * 3.14 * 2;
-    const differentAngel = (90/360) * 3.14 * 2;
-    const salientAngel = (135/360) * 3.14 * 2;
-
-    const chartData: ChartData = { 
-      descriptionTextSize: 8,
-      descriptionTextWeight: 400,
-      sectorLabelsTextSize: 8,
-      sectorLabelsTextWeight: 600,
-      radialSectors: [
-        {
-          sectorStartAngel: 0,
-          sectorEndAngel: salientAngel, 
-          color: '#78C0CF',
-          labelColor: '#000000',
-          descriptionText: 'SALIENT',
-          radialLevels: [
-            { radius: 20, color: '#78C0CF' }, 
-            { radius: 45, color: '#55B0C3', value: brandHealt.salient }
-          ]
-        },
-        {
-          sectorStartAngel: salientAngel,
-          sectorEndAngel: salientAngel + differentAngel, 
-          color: '#65878A',
-          labelColor: '#FFFFFF',
-          descriptionText: 'DIFFERENT',
-          radialLevels: [
-            { radius: 20, color: '#65878A', value: brandHealt.different}
-          ],
-        },
-        {
-          sectorStartAngel: salientAngel + differentAngel, 
-          sectorEndAngel: salientAngel + differentAngel + meaningFulAngel,
-          color: '#A493C9',
-          labelColor: '#FFFFFF',
-          descriptionText: 'MEANINGFUL',
-          radialLevels: [
-            { radius: 20, color: '#A493C9' },
-            { radius: 30, color: '#8C7BAE', value: brandHealt.meaningful}
-          ]
-        },  
-      ], 
-      innerCircle: { 
-        descriptionText: 'POWER',
-        labelColor: '#FFFFFF',
-        radius: 15,
-        value: brandHealt.power,
-        color: '#46393C' 
-      } 
-    };
-
-    return chartData;
   }
 
 }
